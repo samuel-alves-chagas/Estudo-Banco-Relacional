c0d5b61111993ab7cc31b12a54224a5149a4c1cb
Samuel Alves Chagas <samuelchagasalves@gmail.com>
2020-07-01 22:31:54 -0300

    ADD.: Inserção na tabela por ID

---
+++ inserirNovosEstados.sql
@@ -0,0 +1,9 @@
+insert into estados (id, nome, sigla, regiao, populacao)
+values (100, 'Novo', 'NV', 'Sul', 2.54)
+
+select * from estados where sigla='NV'
+
+insert into estados (nome, sigla, regiao, populacao)
+values ('Mais novo', 'MN', 'Norte', 5.2)
+
+select * from estados where sigla='MN'
